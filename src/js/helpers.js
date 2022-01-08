5a533706901bc7d31b27322beb6908be03f1510a
ABHAY0PARIHAR <64008676+ABHAY0PARIHAR@users.noreply.github.com>
2022-01-09 03:33:00 +0530

    Adding JSDoc comments for developers (i)

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,13 +1,25 @@
 import { async } from 'regenerator-runtime';
 import * as config from './config.js';
 
-const timeout = function (s) {
+/**
+ * 
+ * @param {number} seconds Time in seconds to wait
+ * @returns Promise which will further used to reject another promise after certain seconds of time
+ */
+const timeout = function (seconds) {
     return new Promise(function (_, reject) {
         setTimeout(function () {
             reject(new Error(`Request took too long! Timeout after ${s} second`));
-        }, s * 1000);
+        }, seconds * 1000);
     });
 };
+
+/**
+ * If you want to make a POST request then give uploadData else give only one agrument to make GET request.
+ * @param {string} url URL of API to make a GET/POST request
+ * @param {object} uploadData - Data for making POST request
+ * @returns Object of the data requested
+*/
 
 export const AJAX = async function (url, uploadData) {
     try {
@@ -28,37 +40,3 @@ export const AJAX = async function (url, uploadData) {
         throw err;
     }
 }
-
-/* export const getJson = async function (url) {
-    try {
-        const fetchPro = fetch(url);
-        const response = await Promise.race([fetchPro, timeout(config.TIMEOUT_SEC)]);
-        const data = await response.json();
-
-        if (!response.ok) throw new Error(`${data.message} (${data.status})`);
-
-        return data;
-    } catch (err) {
-        throw err;
-    }
-}
-
-export const sendJson = async function (url, uploadData) {
-    try {
-        const fetchPro = fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(uploadData),
-        });
-        const response = await Promise.race([fetchPro, timeout(config.TIMEOUT_SEC)]);
-        const data = await response.json();
-
-        if (!response.ok) throw new Error(`${data.message} (${data.status})`);
-
-        return data;
-    } catch (err) {
-        throw err;
-    }
-} */
--- src/js/model.js
+++ src/js/model.js
@@ -2,6 +2,10 @@ import { async } from 'regenerator-runtime';
 import * as config from './config.js';
 import { AJAX } from './helpers.js';
 
+/**
+ * An object to store recipe which will be displayed, bookmarks of user and the list of recipes which user searched for.
+ */
+
 export const state = {
     recipe: {},
     search: {
@@ -12,6 +16,12 @@ export const state = {
     },
     bookmarks: [],
 }
+
+/**
+ * 
+ * @param {object} data An object that generated from a promise and contains data for a specific recipe which user wants to display in detailed view.
+ * @returns An abject that contains all relevent information about recipe which will be displayed
+ */
 
 const createRecipeObejct = function (data) {
     let { recipe } = data.data;
@@ -27,6 +37,11 @@ const createRecipeObejct = function (data) {
         ...(recipe.key && { key: recipe.key }),
     }
 }
+
+/**
+ * This function makes an AJAX call to API for the particular recipe data. 
+ * @param {string} id ID of the recipe which is to be displyed in detailed view
+ */
 
 export const loadRecipe = async function (id) {
     try {
@@ -46,6 +61,11 @@ export const loadRecipe = async function (id) {
     }
 }
 
+/**
+ * This function makes an AJAX call to API for all recipes that contains given query in their name/title.
+ * @param {string} query Search query for which the recipe reults to be displayed
+ */
+
 export const loadSearchResults = async function (query) {
     try {
         const data = await AJAX(`${config.API_URL}?search=${query}&key=${config.KEY}`);
@@ -60,11 +80,16 @@ export const loadSearchResults = async function (query) {
                 ...(recipe.key && { key: recipe.key }),
             };
         });
-        console.log(data);
     } catch (err) {
         throw err;
     }
 }
+
+/**
+ * This function is basically helpful in making pagination view in User Interface.
+ * @param {number} page The page number which is to be displayed on screen, optional
+ * @returns An array for all recipes which will be displyed on screen
+ */
 
 export const getSearchResultsPage = function (page = state.search.page) {
     state.search.page = page;
@@ -75,6 +100,11 @@ export const getSearchResultsPage = function (page = state.search.page) {
     return state.search.results.slice(start, end);
 }
 
+/**
+ * This function basically sontrols the amount of ingredients to be desplayed.
+ * @param {number} newServings The number of servings taken from UI
+ */
+
 export const updateServings = function (newServings) {
     state.recipe.ingredients.forEach(ingredient => {
         ingredient.quantity = (ingredient.quantity) * newServings / state.recipe.servings;
@@ -83,9 +113,18 @@ export const updateServings = function (newServings) {
     state.recipe.servings = newServings;
 }
 
+/**
+ * Updating bookamrks in local storage to display when page loads
+ */
+
 const persistBookmarks = function () {
     localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
 }
+
+/**
+ * This function add recipe to bookmarks and call to update the local storage
+ * @param {object} recipe Obejct containing recipe details to store in bookmarks array for future access
+ */
 
 export const addBookmark = function (recipe) {
     state.bookmarks.push(recipe);
@@ -94,6 +133,11 @@ export const addBookmark = function (recipe) {
 
     persistBookmarks();
 }
+
+/**
+ * This function searches for the recipe with given ID and delete from the bookmarks and call for update the local storage
+ * @param {number} id ID of the recipe to be removed from bookmarks
+ */
 
 export const deleteBookmark = function (id) {
     const index = state.bookmarks.findIndex(el => el.id === id);
@@ -104,20 +148,33 @@ export const deleteBookmark = function (id) {
     persistBookmarks();
 }
 
+/**
+ * This function basically access bookamrks from local storage to display in bookamrks bar of the web page
+ */
+
 const init = function () {
     const storage = localStorage.getItem('bookmarks');
 
     if (!storage) return;
 
     state.bookmarks = JSON.parse(storage);
-    console.log(state.bookmarks);
 }
 
 init();
 
+/**
+ * This function is for developer use and for developing purpose only.
+ */
+
 const clearBookmarks = function () {
     localStorage.removeItem('bookmarks');
 }
+
+/**
+ * This function make a POST request to API with a developer key. 
+ * This function stores the user generated recipe data on API server so it can be accessed by the user when he uses that key.
+ * @param {object} newRecipe Object which contains every relevent information about new recipe
+ */
 
 export const uploadRecipe = async function (newRecipe) {
     try {
